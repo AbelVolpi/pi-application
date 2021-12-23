ff3cb535b55b8658b54eee25fb814ba8bfc4c4e3
AbelVolpi <abelvolpi@gmail.com>
2021-12-23 18:44:42 -0300

    feat: add Constants archive

--- app/src/main/java/com/example/testingfirebase/utils/Constants.kt
+++ app/src/main/java/com/example/testingfirebase/utils/Constants.kt
@@ -2,6 +2,10 @@ package com.example.testingfirebase.utils
 
 object Constants {
 
+    const val COLLECTION = "campaigns"
+    const val CAMPAIGN_NAME = "campaign_name"
+    const val CAMPAIGN_ADDRESS = "campaign_address"
+    const val CAMPAIGN_CATEGORY = "campaign_category"
 
 
 }
