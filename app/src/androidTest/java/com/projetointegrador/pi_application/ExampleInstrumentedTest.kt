24bdc06473043f4a966c0e22a570a32db12041e7
AbelVolpi <abelvolpi@gmail.com>
2022-01-07 17:58:21 -0300

    fix: missing change name of project in some files

--- app/src/androidTest/java/com/projetointegrador/pi_application/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/projetointegrador/pi_application/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.testingfirebase", appContext.packageName)
+        assertEquals("com.example.pi_application", appContext.packageName)
     }
 }
