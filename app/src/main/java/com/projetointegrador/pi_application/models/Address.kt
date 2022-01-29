46573964c9f6ca37a9e5aafa912c1ba53839fc13
AbelVolpi <abelvolpi@gmail.com>
2022-01-29 19:19:08 -0300

    feat: make campaign parcelable

--- app/src/main/java/com/projetointegrador/pi_application/models/Address.kt
+++ app/src/main/java/com/projetointegrador/pi_application/models/Address.kt
@@ -1,9 +1,13 @@
 package com.projetointegrador.pi_application.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Address(
     val street: String = "",
     var number: String = "",
     var district: String = "",
     var city: String = "",
     var state: String = ""
-)
+) : Parcelable
