7e2daabe9799081ffd4394a49866adadd60aff3c
abelvolpi <abelvolpi@gmail.com>
2026-05-11 19:42:18 -0300

    fix: create campaign

--- app/src/main/java/com/projetointegrador/pi_application/presentation/viewmodel/CreateCampaignViewModel.kt
+++ app/src/main/java/com/projetointegrador/pi_application/presentation/viewmodel/CreateCampaignViewModel.kt
@@ -3,36 +3,29 @@ package com.projetointegrador.pi_application.presentation.viewmodel
 import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.projetointegrador.pi_application.core.base.BaseViewModel
+import androidx.lifecycle.liveData
 import com.projetointegrador.pi_application.core.utils.FirebaseResponse
 import com.projetointegrador.pi_application.domain.models.Campaign
 import com.projetointegrador.pi_application.domain.usecases.campaign.CreateCampaignUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltViewModel
 class CreateCampaignViewModel
     @Inject
     constructor(
-        private val createCampaignUseCase: CreateCampaignUseCase,
-    ) : BaseViewModel() {
+        private val createCampaignUseCase: CreateCampaignUseCase
+    ) : androidx.lifecycle.ViewModel() {
         fun createCampaign(
             campaign: Campaign,
-            imageUri: Uri?,
-        ): LiveData<FirebaseResponse<Boolean>> {
-            val mutableLiveData = MutableLiveData<FirebaseResponse<Boolean>>()
-
-            launch {
+            imageUri: Uri?
+        ): LiveData<FirebaseResponse<Boolean>> =
+            liveData {
                 try {
-                    mutableLiveData.value = createCampaignUseCase.createCampaign(campaign, imageUri).value
-                    // todo adjust this part
+                    emitSource(createCampaignUseCase.createCampaign(campaign, imageUri))
                 } catch (throwable: Throwable) {
-                    Log.e("Create campaign", throwable.message.toString())
-                    mutableLiveData.value = FirebaseResponse.Failure(throwable.message.toString())
+                    Log.e("CreateCampaign", throwable.message.toString())
+                    emit(FirebaseResponse.Failure(throwable.message.toString()))
                 }
             }
-            return mutableLiveData
-        }
     }
